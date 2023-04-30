ab71d153a239f3e25b46b5ee2d248670c623f982
rare200327 <435897971@qq.com>
2023-04-30 10:39:23 +0800

    冒泡排序  bubble_sort

--- test_4.c
+++ test_4.c
@@ -1,8 +1,46 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-//√∞≈›≈≈–Ú
+//√∞≈›≈≈–Ú  bubble_sort
 #include <stdio.h>
+void bubble_sort(int arr[], int sz)
+{
+	int i, j;  //»∑∂®ÃÀ ˝ sz-1 ÃÀ
+	for (i = 0; i < sz-1 ; i++)
+	{
+		//“ªÃÀ√∞≈›≈≈–Úµƒπ˝≥Ã
+		for (j = 0; j < sz - 1 -i ; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
 int main()
 {
-
+	//int arr[] = { 10,9,8,7,6,5,4,3,2,1,0 };  //0 1 2 3 4 5 6 7 8 9 10
+	int arr[] = { 2,8,9,2,3,5,1,0 };           //0 1 2 2 3 5 8 9
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr,sz);
+	//printf("%d ", arr);
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
